f809af259b81e6391c470bb7009a8f0626577918
Swapnil639 <swapnilpatil631997@gmail.com>
2022-03-31 10:01:09 +0530

    initial command

---
+++ selectloop.sh
@@ -0,0 +1,3 @@
+echo "How many planets are there in solar system?"
+
+select op in 6 8 9 10
---
+++ whileloop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+cnt=0;
+
+while [ $cnt -lt 5 ]
+do 
+      echo $cnt
+      ((cnt++));
+done
